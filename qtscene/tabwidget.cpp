a9608115a625ae931f76b26996b500926fcfdb49
liangman <liangman@obook.com.cn>
2020-11-12 17:03:48 +0800

    新文件：   close.png 	修改：     tabs.qrc 	修改：     tabwidget.cpp

--- qtscene/tabwidget.cpp
+++ qtscene/tabwidget.cpp
@@ -5,6 +5,9 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QHBoxLayout>
+#include <QIcon>
+#include <QTabBar>
+#include <QPalette>
 
 Tabwidget::Tabwidget(QWidget *parent) : QWidget(parent)
 {
@@ -17,6 +20,25 @@ Tabwidget::Tabwidget(QWidget *parent) : QWidget(parent)
 void Tabwidget::initface()
 {
     QTabWidget *tabWidget = new QTabWidget(this);
+    //tabWidget->setTabShape(QTabWidget::Triangular);//设置选项卡的形状 Rounded
+    tabWidget->setMovable(true);  //设置表头可以拖动换位置
+    tabWidget->setTabPosition(QTabWidget::North);
+    tabWidget->setIconSize(QSize(20,20));
+    tabWidget->setTabsClosable(true);
+
+    QIcon iconClose(":/image/close.png");
+    QPushButton *closeBut = new QPushButton(this);
+    //closeBut->setGeometry(0,0,0,0);
+    closeBut->setIcon(iconClose);
+
+    QPalette pal = closeBut->palette();              //startBtn是我已经定义好的QPushButton对象
+    pal.setColor(QPalette::ButtonText, Qt::red);    //设置按钮上的字体颜色，理论上可以，实际上就是可以
+    pal.setColor(QPalette::Button, Qt::green);      //设置按钮背景颜色，理论上可以，实际上不可以
+
+    closeBut->setPalette(pal);
+    closeBut->setFixedSize(15,15);
+    //closeBut.setStyleSheet("background-color:green");
+
 
     //新建第一个页面的部件
     QWidget *widget = new QWidget(this);
@@ -27,19 +49,23 @@ void Tabwidget::initface()
     vLayout->addWidget(pushButton);
 
     //向QTabWidget中添加第一个页面
-    //QIcon icon1(":/new/icon/images/1.ico");
+    //QIcon icon1(":/image/1.png");
+    //tabWidget->addTab(widget,icon1,"Tab1");
     tabWidget->addTab(widget,"Tab1");
+    //((QTabBar*)(tabWidget->tabBar()))->setTabButton(tabWidget->indexOf(widget),QTabBar::RightSide,NULL);
+    //((QTabBar*)(tabWidget->tabBar()))->setTabButton(tabWidget->indexOf(widget),QTabBar::RightSide,closeBut);
 
     //新建第二个页面的部件
     QLabel *label = new QLabel("Hello Qt",this);
-    //QIcon icon2(":/new/icon/images/2.ico");
+    //QIcon icon2(":/image/2.png");
+    //tabWidget->addTab(label,icon2,"Tab2");
     tabWidget->addTab(label,"Tab2");
 
     //新建第三个页面的部件
     QPushButton *pushButton3 = new QPushButton("Click Me",this);
-    //QIcon icon3(":/new/icon/images/3.ico");
+    //QIcon icon3(":/image/3.png");
+    //tabWidget->addTab(pushButton3,icon3,"Tab3");
     tabWidget->addTab(pushButton3,"Tab3");
-
 
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(tabWidget);
